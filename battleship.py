1702f5f464c47abef6a541c628eb91192d3729eb
WANDEX <downloadfils@gmail.com>
2017-09-29 02:10:38 +0300

    test commit

---
+++ battleship.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+
+def c_dir_list():
+    message = "current directory contains next files:"
+    c_dir_list = str(os.listdir())
+    output = message + "\n" + c_dir_list
+    print(output)
+    return output
+
+c_dir_list()
+os.makedirs("temp_dir", mode=0o777, exist_ok=True)
+print("new call after update")
+c_dir_list()
