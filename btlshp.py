1ca73d7beaa08e0db138fb6d6d1da08cd15808da
WANDEX <downloadfils@gmail.com>
2018-02-07 13:52:19 +0300

    added input_turn()

--- btlshp.py
+++ btlshp.py
@@ -2,14 +2,28 @@
 
 
 def draw_cells():
-    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-    numbers = range(1, 10)
+    A = '\t'.expandtabs(3) + 'A'
+    letters = [A, 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    numbers = range(1, 11)
     for x in letters:
-        print(x, end=' ')
+        print(x, end='')
 
     print('')
+    col = 'o' * 10
+
     for y in numbers:
-        print(y)
+        print(str(y) + '\t'.expandtabs(2) + (col))
+
+
+def input_turn():
+    print()
+    print('col: ')
+    x = input()
+    print('row: ')
+    y = input()
+
+    print('turn is: x = {} y = {}'.format(x, y))
 
 
 draw_cells()
+input_turn()
