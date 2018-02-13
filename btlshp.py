8dfbd0670ebfdb37048f9f8925503bd7eac0d67f
WANDEX <downloadfils@gmail.com>
2018-02-13 20:59:11 +0300

    hit detection bugfix

--- btlshp.py
+++ btlshp.py
@@ -28,15 +28,16 @@ def print_board(board):
 
 
 def random_row(board):
-    return randint(0, len(board) - 1)
+    return randint(1, len(board))
 
 
 def random_col(board):
-    return randint(0, len(board) - 1)
+    return randint(1, len(board))
 
 
 ship_row = random_row(board)
 ship_col = random_col(board)
+
 
 
 # Everything from here on should go in your for loop!
@@ -49,20 +50,22 @@ while turn < 5:
     print_letters()
     print_board(board)
 
-    guess_row = int(input("Guess Row: ")) #- 1
-    guess_col = int(input("Guess Col: ")) #+ 2
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
 
     if guess_row == ship_row and guess_col == ship_col:
         print("Congratulations! You sunk my battleship!")
         board[guess_row - 1][guess_col + 2] = "X"
     else:
+        guess_row = guess_row - 1
+        guess_col = guess_col + 2
         if((guess_row < 0 or guess_row > 10) or
            (guess_col < 0 or guess_col > 10)):
             print("Oops, that's not even in the ocean.")
-        elif((board[guess_row - 1][guess_col + 2] == ".") or
-             (board[guess_row - 1][guess_col + 2] == "X")):
+        elif((board[guess_row][guess_col] == ".") or
+             (board[guess_row][guess_col] == "X")):
             print("You guessed that one already.")
         else:
             print("You missed my battleship!")
-            board[guess_row - 1][guess_col + 2] = "."
+            board[guess_row][guess_col] = "."
         print()
